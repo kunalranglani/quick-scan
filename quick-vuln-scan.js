63b88bc8d799ffe166e9d81390a93a1645e14f3f
Kunal Ranglani <kunalr@microsoft.com>
2026-07-24 12:26:26 -0700

    add a new function

--- quick-vuln-scan.js
+++ quick-vuln-scan.js
@@ -18,7 +18,18 @@ function buildQuery(userId) {
   return "SELECT * FROM users WHERE id = '" + userId + "'";
 }
 
+function authenticate(username, password = "DefaultP@ssw0rd!") {
+  // Vulnerability: default password embedded in function definition.
+  return { username, password, authenticated: true };
+}
+
 // Vulnerability: hardcoded credential/secret (CWE-798).
 const HARDCODED_PASSWORD = "P@ssw0rd123!";
 
-module.exports = { runUserCode, runCommand, buildQuery, HARDCODED_PASSWORD };
+module.exports = {
+  runUserCode,
+  runCommand,
+  buildQuery,
+  authenticate,
+  HARDCODED_PASSWORD,
+};
